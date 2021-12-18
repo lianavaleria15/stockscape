492f23af3ad63ccd9b62685046f11a434b668e03
liana.valeria15 <liana.valeria15@gmail.com>
2021-12-18 13:50:48 +0000

    added company model

--- src/models/company.js
+++ src/models/company.js
@@ -1,1 +1,36 @@
+const { Model, DataTypes } = require("sequelize");
 
+const sequelize = require("../config/connection.js");
+
+class Company extends Model {}
+
+Company.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+
+    name: { type: DataTypes.STRING, allowNull: "false" },
+
+    investment_profile_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "investment-profile",
+        key: "id",
+      },
+    },
+  },
+
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "category",
+  }
+);
+
+module.exports = { Company };
