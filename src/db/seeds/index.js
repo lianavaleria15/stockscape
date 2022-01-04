cefcdf9d8d1122960146ab95df141f2d3067aeed
Kayle Rieger Patton <kayle.patton22@gmail.com>
2022-01-04 19:47:08 +0000

    user seeds

--- src/db/seeds/index.js
+++ src/db/seeds/index.js
@@ -1,7 +1,7 @@
-// const seedCategories = require("./category-seeds");
-// const seedProducts = require("./product-seeds");
-// const seedTags = require("./tag-seeds");
-// const seedProductTags = require("./product-tag-seeds");
+const seedUsers = require("./category-seeds");
+const seedCompanies = require("./product-seeds");
+const seedInvestmentProfiles = require("./tag-seeds");
+
 //user
 //investment
 //companies
