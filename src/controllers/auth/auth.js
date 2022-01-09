561e63d6d54cd3468f8c6d936e1d96c1363eb59e
Kayle Rieger Patton <kayle.patton22@gmail.com>
2022-01-09 10:04:39 +0000

    signup controller fn

--- src/controllers/auth/auth.js
+++ src/controllers/auth/auth.js
@@ -5,8 +5,35 @@ const {
 } = require("../../helpers/utils");
 
 // /auth/ endpoints
-const signup = (req, res) => {};
+const signup = (req, res) => {
+  try {
+    // get payload
+    const payload = getPayloadWithValidFieldsOnly(
+      ["username", "password", "email", "firstName", "lastName"],
+      req.body
+    );
+
+    // if not all payload fields are present, throw error
+    if (Object.keys(payload).length !== 5) {
+      return res
+        .status(400)
+        .json({ success: false, error: "Please provide the valid fields." });
+    }
+
+    // create new user record w/ payload
+    await User.create(payload);
+
+    return res.json({ success: true, message: "Created new user." });
+  } catch (error) {
+    logError("POST user", error.message);
+    return res
+      .status(500)
+      .json({ success: false, error: "Failed to create user." });
+  }
+};
+
 const login = (req, res) => {};
+
 const logout = (req, res) => {
   // if user is logged in, destroy session
   if (req.session.loggedIn) {
