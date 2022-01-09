6f4a51c5d55537f738a40adea81c861519f448a8
Kayle Rieger Patton <kayle.patton22@gmail.com>
2022-01-09 22:37:21 +0000

    update private route paths

--- src/routes/view/privateRoutes.js
+++ src/routes/view/privateRoutes.js
@@ -11,11 +11,12 @@ const router = Router();
 
 // Private / endpoints
 router.get("/dashboard", renderDashboard);
-// profile/edit/:id
-router.get("/edit-profile/:id", renderEditMyProfile);
-// investmentportfolio
-router.get("/create-portfolio", renderCreateMyPortfolio);
-// investmentportfolio/:id
-router.get("/edit-portfolio/:id", renderEditMyPortfolio);
+
+router.get("/:id/profile/edit", renderEditMyProfile);
+
+router.get("/:id/portfolio/create", renderCreateMyPortfolio);
+
+router.get("/portfolio/:id/edit/", renderEditMyPortfolio);
+// OR router.get("/:id/portfolio/:id/edit/", renderEditMyPortfolio);
 
 module.exports = router;
