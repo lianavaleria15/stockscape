b7b379a02ec57d2d6cd94e363ff6a7e3fbf00261
Conor Kelly <conorjkelly96@gmail.com>
2022-01-12 19:34:12 +0000

    code cleanup - pending migration

--- public/assets/js/signup.js
+++ public/assets/js/signup.js
@@ -1,52 +1,5 @@
-// jQuery Sign Up Elements
 const signupForm = $("#signup-form");
 const alreadyExistsModal = $("#already-exists-modal");
-
-// jQuery Login Elements
-const loginForm = $("#login-form");
-const doesntExistModal = $("#doesnt-exist-modal");
-
-// jQuery Logout Elements
-// const logoutBtn = $("#logout-btn");
-
-const handleLogin = async (event) => {
-  //   prevent form default
-  event.preventDefault();
-
-  // get post body from form fields
-  const username = $("#username").val();
-  const password = $("#password").val();
-
-  // CODE ERROR MESSAGES FOR EMPTY FIELDS
-  const errors = getErrorsLogIn({
-    username,
-    password,
-  });
-
-  renderErrorMessages(errors);
-
-  // make POST request to /auth/login
-  const response = await fetch("/auth/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ username, password }),
-  });
-
-  const data = await response.json();
-
-  if (data.error === "Username does not exist") {
-    console.log("USER DOES NOT EXIST");
-    doesntExistModal.modal("show");
-  }
-
-  if (data.success) {
-    console.log("Logged in");
-    // direct to dashboard
-    window.location.replace("/homepage");
-  }
-};
 
 const getErrorsSignUp = ({
   email,
@@ -93,42 +46,6 @@ const getErrorsSignUp = ({
   }
 
   return errors;
-};
-
-const getErrorsLogIn = ({ username, password }) => {
-  const errors = {};
-
-  if (!password) {
-    const error = (errors.password = "Invalid password");
-    console.log(error);
-  }
-
-  if (!username) {
-    const error = (errors.username = "Username is required");
-    console.log(error);
-  }
-
-  return errors;
-};
-
-const renderErrorMessages = (errors) => {
-  const fields = [
-    "email",
-    "password",
-    "firstName",
-    "lastName",
-    "username",
-    "confirmPassword",
-  ];
-  fields.forEach((field) => {
-    const errorDiv = $(`#${field}-error`);
-
-    if (errors[field]) {
-      errorDiv.text(errors[field]);
-    } else {
-      errorDiv.text("");
-    }
-  });
 };
 
 const handleSignup = async (event) => {
@@ -184,24 +101,3 @@ const handleSignup = async (event) => {
     }
   }
 };
-
-const handleLogout = async () => {
-  const response = await fetch("/auth/logout", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-
-  const data = await response.json();
-
-  if (data.success) {
-    //   send to homepage
-    window.location.replace("/");
-  }
-};
-
-// EVENT LISTENERS
-signupForm.on("submit", handleSignup);
-loginForm.on("submit", handleLogin);
-// logoutBtn.on("click", handleLogout);
