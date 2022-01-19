6dd6a9abc4678606d095fbb4cf60c043ba27bc9d
Conor Kelly <conorjkelly96@gmail.com>
2022-01-19 21:43:31 +0000

    absolutely mental

--- public/assets/js/companies-page.js
+++ public/assets/js/companies-page.js
@@ -1,36 +1,76 @@
 //get view more btn
 const companiesContainer = $("#companies-container");
 
-const renderStockInfoModal = (event) => {
+const constructPortfolioOptions = (portfolios) => {
+  return portfolios
+    .map((portfolio) => {
+      return `<option id="${portfolio.id}" value="${portfolio.name}">${portfolio.name}</option>`;
+    })
+    .join("");
+};
+const constructCompanyModal = ({ company, portfolios }) => {
+  return `<div class="modal fade" id="company-card-modal" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header bg-success text-white">
+          <h5 class="modal-title" id="company-name">${company.name}</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <h6 class="card-subtitle">${company.sector}</h6>
+          <p>${company.symbol}</p>
+          <p>January 2021 share price: <span>${company.janPrice}</span></p>
+          <p>${company.company_summary}</p>
+        </div>
+        <form id="add-company-form">
+          <div class="modal-footer d-flex justify-content-between">
+            <div>
+              <label for="number-shares"> Number of shares:</label>
+              <input type="text" name="" id="number-shares" />
+            </div>
+            <div>
+              <label for="portfolio-name">Select a portfolio:</label>
+              <select name="portfolio-name" id="portfolio-name">
+                ${constructPortfolioOptions(portfolios)}
+              </select>
+            </div>
+            <div>
+              <button type="submit" class="btn btn-secondary" data-bs-dismiss="modal">Add to
+                portfolio</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>`;
+};
+
+const renderStockInfoModal = async (event) => {
   const target = event.target;
 
-  if ($(target).is("button")) {
-    const id = $(target).attr("id");
+  if ($(target).is('button[name="view-company"]')) {
+    if ($("#company-card-modal").length) {
+      $("#company-card-modal").remove();
+    }
 
-    $(`[data-id=${id}]`).modal("show");
+    const companyId = $(target).attr("id");
+
+    // make API request
+    const response = await fetch(`/api/companies/${companyId}`);
+
+    const { data } = await response.json();
+
+    const companyModal = constructCompanyModal(data);
+
+    companiesContainer.append(companyModal);
+    $("#company-card-modal").modal("show");
+    $("#add-company-form").on("submit", addCompanyToPortfolio);
   }
-};
-
-const renderPortfolioPage = () => {
-  console.log("hooray");
-};
-
-//initialize local storage
-const initializeLocalStorage = () => {
-  //get portfolios from LS
-  const basketPortfolios = JSON.parse(localStorage.getItem("portfolios")) ?? [];
-
-  //set portfolios to LS
-  localStorage.setItem("portfolios", JSON.stringify(basketPortfolios));
-
-  return basketPortfolios;
 };
 
 //event listener on company modal
 const addCompanyToPortfolio = (event) => {
   event.preventDefault();
-
-  initializeLocalStorage();
 
   const numberShares = $("#number-shares").val();
 
@@ -40,19 +80,19 @@ const addCompanyToPortfolio = (event) => {
 
   const companyName = $("#company-name").text();
 
-  //store new portfolio data
-  const newPortfolioToBasket = { companyName, numberShares };
+  // //store new portfolio data
+  // const newPortfolioToBasket = { companyName, numberShares };
 
-  //push new portfolio data to portfolio array
-  basketPortfolios.push(newPortfolioToBasket);
+  console.log(numberShares);
 
-  //add to LS
-  localStorage.setItem("portfolios", JSON.stringify(basketPortfolios));
-  //portfolio name key to LS, value array of objects with (company name, nr shares)
+  $("#company-card-modal").on("hide.bs.modal", () => {
+    console.log("closing modal");
+  });
 };
 
 //event on view more btn
 companiesContainer.on("click", renderStockInfoModal);
 
 //event on modal to add to stockbasket
-$("#added-to-basket").on("submit", addCompanyToPortfolio);
+// const modalId = $("#company-card-modal").data("id");
+// $("#add-company-form").on("submit", addCompanyToPortfolio);
--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 
 const userRoutes = require("./userRoutes");
 const portfolioRoutes = require("./portfolioRoutes");
+const companiesRoutes = require("./companiesRoutes");
 const apiAuth = require("../../middlewares/apiAuth");
 
 const router = Router();
@@ -9,5 +10,6 @@ const router = Router();
 //  api routes
 router.use("/users", apiAuth, userRoutes);
 router.use("/portfolios", apiAuth, portfolioRoutes);
+router.use("/companies", apiAuth, companiesRoutes);
 
 module.exports = router;
